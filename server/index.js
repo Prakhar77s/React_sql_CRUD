6cf9a2e33b130c05c85c3291902e9bbe72d2f543
Prakhar Singh <prakhars76@gmail.com>
2022-05-25 16:33:23 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "crud_database",
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(result);
+  });
+});
+
+app.get("/", (req, res) => {});
 
 app.listen(3001, () => {
   console.log("server running");
